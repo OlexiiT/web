0077c6fe1abef76628ee3f8f49ebd1a57b27f7a5
Olexii <75754524+OlexiiT@users.noreply.github.com>
2023-02-03 23:36:02 +0200

    Add files via upload

---
+++ test/spring/testApp1/src/main/java/com/example/testapp1/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.testapp1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    String hello() {
+        return "hello";
+    }
+}
